f703ab0b74b9bd8634fee577ad6294b93cb64c97
M.S.T.O.P <mstop4@gmail.com>
2019-03-26 18:37:09 -0400

    set ENV variables

--- config/application.rb
+++ config/application.rb
@@ -15,5 +15,14 @@ module Blog
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+
+    # Load ENV variables 
+    config.before_configuration do
+      env_file = File.join(Rails.root, 'config', 'local_env.yml')
+      YAML.load(File.open(env_file)).each do |key, value|
+        ENV[key.to_s] = value
+      end if File.exists?(env_file)
+    end
+
   end
 end
